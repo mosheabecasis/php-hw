752dbc4551617368a9f9da87cda4b754cef480b9
moshe abecasis <mosh14abou@yahoo.com>
2021-03-04 19:11:37 +0200

    a3

--- index3.php
+++ index3.php
@@ -1,17 +1,14 @@
 <!-- EX4 -->
 <?php
 $array = array("1", "2");
-function change($array)
-{
-    $i = 1;
-    $newarray = [];
-    foreach ($array as $n) {
-        $newarray[$i] = $n;
-        $i--;
-    }
+function change($anarray){
+     $holder1 = $anarray[0];
+     $holder2 = $anarray[1];
+     $newarray = array($holder2,$holder1);
     var_dump($newarray);
-    var_dump($array);
-    return $newarray;
 }
 change($array);
+
 ?>
+
+
